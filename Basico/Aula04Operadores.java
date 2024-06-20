33a69c914b596fcbf527f4f364dc837257ab3ed2
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-06-19 21:54:00 -0300

    .

---
+++ Basico/Aula04Operadores.java
@@ -0,0 +1,11 @@
+package Basico;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        float n1 = 10f;
+        float n2 = 21f;
+        double s = n1 / n2;
+        float r = n2%n1;
+        System.out.println("Soma: "+s+" Resto: " + r);
+    }
+}
---
+++ Basico/Aula05EstruturaCondicionais.java
@@ -0,0 +1,4 @@
+package Basico;
+
+public class Aula05EstruturaCondicionais {
+}
