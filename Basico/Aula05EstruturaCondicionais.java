5f3f17308041073128333442d251297221418227
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-06-19 22:08:18 -0300

    ;

--- Basico/Aula05EstruturaCondicionais.java
+++ Basico/Aula05EstruturaCondicionais.java
@@ -1,4 +1,12 @@
 package Basico;
 
 public class Aula05EstruturaCondicionais {
+    public static void main(String[] args) {
+        int idade = 20;
+        if(idade >= 20){
+            System.out.println("Idade maior ou igual a 20");
+        }else {
+            System.out.println("Idade menor que 20");
+        }
+    }
 }
