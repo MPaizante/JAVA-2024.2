a7ac9ff10403d21feff5b02b09cd913d7dbe1b1f
Matheus Paizante <119441065+MPaizante@users.noreply.github.com>
2024-06-24 23:47:16 -0300

    ;

--- Basico/Aula10Arrays01.java
+++ Basico/Aula10Arrays01.java
@@ -9,5 +9,24 @@ public class Aula10Arrays01 {
         for (int i = 0; i < idades.length; i++) {
             System.out.println(idades[i]);
         }
-    }
+        String [] nomes = new String[4];
+        nomes[0] = "A";
+        nomes[1] = "B";
+        nomes[2] = "C";
+        nomes[3] = "D";
+        for (int i = 0; i < nomes.length; i++) {
+            System.out.println(nomes[i]);
+        }
+
+        System.out.println("=============================================");
+        int [] n = new int[3];
+        int [] n2 = {1,2,3,4,5};
+        int [] n3 = new int[]{6,7,8,9,10};
+
+        for (int i = 0; i < n3.length; i++) {
+            System.out.println(n3[i]);
+
+        }
+        
+}
 }
